15ce6a1ad019be0925ddc04b8ea3f0f7cabfb54a
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-11-15 21:11:22 +0200

    useFetch function added, axios added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import {BrowserRouter as Router} from "react-router-dom"
-import AllRoutes from "./node_modules/routes.jsx"
-import TopBar from "./node_modules/components/topBar.jsx"
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AllRoutes from "./node_modules/routes.jsx";
+import TopBar from "./node_modules/components/topBar.jsx";
 function App() {
-	return (
-		<div>
-			<Router>
-				<TopBar></TopBar>
-				<AllRoutes></AllRoutes>
-			</Router>
-		</div>
-	)
+  return (
+    <div>
+      <Router>
+        <TopBar></TopBar>
+        <AllRoutes></AllRoutes>
+      </Router>
+    </div>
+  );
 }
-export default App
+export default App;
