5d60aa96174ecbc5c37e94833db865a993ee8e40
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-11-18 15:29:51 +0200

    feed page created, now it is availiable to see all posts

---
+++ src/dist/components/feed.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Feed = ({ articles }) => {
+  const noImage =
+    'https://lh3.googleusercontent.com/proxy/AGxe1nsJs24WYUgCDGglnJzaADeHC0VaA_YxJQW76UUmAzsP_l0Y5PL4y6pVcEAa2ihchYtwlsRoiji9lRzgMxQo4lJ1Y5DvSP1vt4loHPyDuVX-y10';
+
+  return (
+    <div>
+      {articles.map((article, index) => (
+        <div className="article-preview" key={index}>
+          <div className="article-meta">
+            <Link to={`/profiles/${article.author.username}`}>
+              <img src={article.author.image === '' ? noImage : article.author.image} alt="" />
+            </Link>
+            <div className="">
+              <Link to={`/profiles/${article.author.username}`} className="author"></Link>
+              <span className="date">{article.createdAt}</span>
+            </div>
+          </div>
+          <Link to={`/articles/${article.slug}`} className="preview-link">
+            <h1>{article.title}</h1>
+            <p>{article.description}</p>
+            <span>Read more...</span>
+            <ul className="tag-list">
+              {article.tagList.map((tag) => (
+                <li key={tag} className="tag-default tag-pill tag-outline"></li>
+              ))}
+            </ul>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Feed;
--- src/dist/pages/globalFeed/index.jsx
+++ src/dist/pages/globalFeed/index.jsx
@@ -1,8 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Feed from '../../components/feed';
+import useFetch from '../../hooks/useFetch';
 
 const GlobalFeed = () => {
-	return (
-		<h1>It's global feed</h1>
-	)
-}
-export default GlobalFeed
+  const apiUrl = '/articles';
+  const [{ response, isLoading, error }, doFetch] = useFetch(apiUrl);
+  console.log(response);
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
+  return (
+    <div className="home-page">
+      <div className="banner">
+        <div className="container">
+          <h1>Go</h1>
+          <p>Educational project</p>
+        </div>
+      </div>
+      <div className="container page">
+        <div className="row">
+          <div className="col-md-9">
+            {isLoading && <div>Loading</div>}
+            {error && <div>Some error happened</div>}
+            {!isLoading && response && <Feed articles={response.articles}></Feed>}
+          </div>
+          <div className="col-md-3">Popular tags</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default GlobalFeed;
