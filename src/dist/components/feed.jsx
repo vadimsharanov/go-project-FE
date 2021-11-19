cdd6b99bed7ee7dd993676c366564715a9fe6e3a
Vadim Šaranov <vadim.sharanov@gmail.com>
2021-11-19 11:04:04 +0200

    single article added

--- src/dist/components/feed.jsx
+++ src/dist/components/feed.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { dateFormat } from '../../utils';
-const Feed = ({ articles }) => {
-  const noImage =
-    'https://lh3.googleusercontent.com/proxy/AGxe1nsJs24WYUgCDGglnJzaADeHC0VaA_YxJQW76UUmAzsP_l0Y5PL4y6pVcEAa2ihchYtwlsRoiji9lRzgMxQo4lJ1Y5DvSP1vt4loHPyDuVX-y10';
+import TagList from './tagList';
 
+const Feed = ({ articles }) => {
   return (
     <div>
       {articles.map((article, index) => (
         <div className="article-preview" key={index}>
           <div className="article-meta">
             <Link to={`/profiles/${article.author.username}`}>
-              <img src={article.author.image === '' ? noImage : article.author.image} alt="" />
+              <img src={article.author.image} alt="" />
             </Link>
             <div className="">
               <Link to={`/profiles/${article.author.username}`} className="author"></Link>
@@ -22,11 +21,7 @@ const Feed = ({ articles }) => {
             <h1>{article.title}</h1>
             <p>{article.description}</p>
             <span>Read more...</span>
-            <ul className="tag-list">
-              {article.tagList.map((tag) => (
-                <li key={tag} className="tag-default tag-pill tag-outline"></li>
-              ))}
-            </ul>
+            <TagList tags={article.tagList}></TagList>
           </Link>
         </div>
       ))}
--- src/dist/pages/article/index.jsx
+++ src/dist/pages/article/index.jsx
@@ -1,8 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router';
+import { Link } from 'react-router-dom';
+import ErrorMessage from '../../components/errorMessage';
+import Loading from '../../components/loading';
+import TagList from '../../components/tagList';
+import useFetch from '../../hooks/useFetch';
 
 const Article = () => {
-	return (
-		<h1>It's article</h1>
-	)
-}
-export default Article
+  const location = useLocation();
+  const slug = location.pathname.split('/articles/').join('');
+  const apiUrl = `/articles/${slug}`;
+  const [{ response, error, isLoading }, doFetch] = useFetch(apiUrl);
+  useEffect(() => {
+    doFetch();
+  }, [doFetch]);
+
+  return (
+    <div className="article-page">
+      <div className="banner">
+        {!isLoading && response && (
+          <div className="container">
+            <h1>{response.article.title}</h1>
+            <div className="article-meta">
+              <Link to={`/profiles/${response.article.author.username}`}>
+                <img src={response.article.author.image} alt="" />
+              </Link>
+              <div className="info">
+                <Link to={`/profiles/${response.article.author.username}`}>
+                  {response.article.author.username}
+                </Link>
+                <span className="date"> {response.article.createdAt}</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="container page">
+        {isLoading && <Loading></Loading>}
+        {error && <ErrorMessage></ErrorMessage>}
+        {!isLoading && response && (
+          <div className="row article-content">
+            <div className="col-xs-12">
+              <div>
+                <p>{response.article.body}</p>
+              </div>
+              <TagList tags={response.article.tagList}></TagList>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Article;
